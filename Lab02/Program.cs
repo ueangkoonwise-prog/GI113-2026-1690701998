6eb639134fd302bb3cae0b42544bd7805d3683a6
ueangkoonwise-prog <ueangkoon.wise@bumail.net>
2026-08-25 10:41:15 +0700

    Add Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID : 1690701998
+ * Name       : Lab02
+ * Section    : 129c
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
